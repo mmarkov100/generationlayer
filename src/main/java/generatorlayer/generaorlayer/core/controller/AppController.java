3282b7369e478596aee22b04e960102fc19fee5b
mmarkov100 <pikola1epta@gmail.com>
2025-02-10 03:56:45 +0500

    Добавлен /app/health

--- src/main/java/generatorlayer/generaorlayer/core/controller/AppController.java
+++ src/main/java/generatorlayer/generaorlayer/core/controller/AppController.java
@@ -1,17 +1,18 @@
 package generatorlayer.generaorlayer.core.controller;
 
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/app")
 public class AppController {
 
     @GetMapping("/health")
-    public ResponseEntity<?> health() {
-        return ResponseEntity.ok().body("OK");
+    public Map<String, String> healthCheck() {
+        return Map.of("status", "UP");
     }
 }
